82b48a711a2485a22fe4ca504197befca502ce42
Pedro Novais <phenrique101007@gmail.com>
2025-08-18 21:22:39 -0300

    Initial Commit

---
+++ src/main/java/com/techlab/techlabweb/TechlabWebApplication.java
@@ -0,0 +1,13 @@
+package com.techlab.techlabweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechlabWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechlabWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techlab/techlabweb/TechlabWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techlab.techlabweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechlabWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
